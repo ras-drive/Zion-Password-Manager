ed00af00b25b07e424b00576a16fccdee7dd22db
Sarah <rasdrive4165@protonmail.com>
2022-08-29 12:10:57 -0500

    added actix web server files

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+// extern crate actix_web;
+
+use actix_files as fs;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use handlebars::Handlebars;
+use serde_json::json;
+
+#[get("/")]
+async fn index(hb: web::Data<Handlebars<'_>>) -> impl Responder {
+    let data = json!({});
+    let body = hb.render("index", &data).unwrap();
+    HttpResponse::Ok().body(body)
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".hbs", "./static/html")
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+
+    println!("listening on port 8080");
+    HttpServer::new(move || {
+        App::new()
+            .app_data(handlebars_ref.clone())
+            .service(
+                fs::Files::new("/public", ".")
+                    .show_files_listing()
+                    .use_last_modified(true),
+            )
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
