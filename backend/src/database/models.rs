5ac67b55087dba459cd6ef8a6ebeee9be63158c4
Sarah <rasdrive4165@protonmail.com>
2022-11-28 18:42:25 -0600

    added a method to get users by id

--- backend/src/database/models.rs
+++ backend/src/database/models.rs
@@ -15,7 +15,7 @@ use pbkdf2::{
 };
 
 
-#[derive(Queryable, Clone, Debug, Insertable)]
+#[derive(Queryable, Clone, Debug, Insertable, Selectable)]
 #[diesel(table_name = users)]
 pub struct User {
     pub id: i32,
@@ -36,11 +36,16 @@ impl User {
         }
     }
 
+    ///
     /// attempts to write self to user database
     ///
     /// user id is rewritten on insert to line up with database
+    /// password_hash is finally rewritten to an encrypted pbkdf2 that can be verified against
+    /// 
     pub async fn insert(&mut self) -> Result<()> {
         let mut conn = establish_connection();
+
+        let password = self.password_hash.clone();
 
         let user_id = users
             .load::<User>(&mut conn)
@@ -64,18 +69,35 @@ impl User {
 
         insert_into(users).values(self.clone()).execute(&mut conn)?;
 
+        self.verify(password).expect("error while verifying password");
+
         Ok(())
     }
 
+    ///
+    /// tests password_hash against an unhashed password string using pbkdf2 crate
+    /// 
     pub fn verify(&self, unhashed_password: String) -> pbkdf2::password_hash::Result<()> {
         let parsed_hash = PasswordHash::new(&self.password_hash).unwrap();
 
         Pbkdf2.verify_password(unhashed_password.as_bytes(), &parsed_hash)
     }
 
+    ///
+    /// gets a result with a User struct from the user table
+    /// 
+    pub fn get_user_by_id(user_id: i32) -> Result<User> {
+        let mut conn = establish_connection();
+
+    match users.filter(users::id.eq(user_id)).load::<User>(&mut conn) {
+        Ok(v) => Ok(v.first().unwrap().clone()),
+        Err(e) => Err(e.into())
+    }
+    }
+
     /// This function mostly exists for test cases and when a user should be deleted a different process should be used which shifts the database.
     ///
-    /// # Safety
+    /// ### Safety
     /// This function panics unless run in a test enviroment
     ///
     pub async unsafe fn delete(&mut self, user_id: i32) -> io::Result<User> {
@@ -109,15 +131,32 @@ impl Default for User {
 
 #[cfg(test)]
 mod tests {
+    use serial_test::serial;
+
     use super::*;
 
     // creates a default test user, inserts them into the DB, verifies the unhashed password
     #[tokio::test]
+    #[serial]
     async fn test_insert_and_delete_user() {
         let mut user = User::default();
         user.insert().await.expect("error inserting user");
 
         user.verify(User::default().password_hash).expect("error while verifying password");
+
+        unsafe {
+            user.delete(user.id).await.expect("error deleting user");
+        }
+    }
+
+    // tests the get_user_by_id method
+    #[tokio::test]
+    #[serial]
+    async fn test_get_user() {
+        let mut user = User::default();
+        user.insert().await.expect("error inserting user");
+
+        User::get_user_by_id(i32::MAX).expect("error retrieving user");
 
         unsafe {
             user.delete(user.id).await.expect("error deleting user");
