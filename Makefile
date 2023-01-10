3ca4194e91ac111c25d42050b353a17a9b4f6189
Sarah <rasdrive4165@protonmail.com>
2023-01-10 17:04:11 -0600

    updated logging to use tracing logger

--- Makefile
+++ Makefile
@@ -10,18 +10,6 @@ run:
 	cd backend; \
 	RUST_LOG=debug cargo run
 
-docker_run:
-	cd backend/migrations; \
-	diesel migration run; \
-	cd ../../backend; \
-	RUST_LOG=debug ADDR=DOCKER cargo run
-
-docker_test:
-	cd backend/migrations; \
-	diesel migration run; \
-	cd ../../backend; \
-	RUST_LOG=debug ADDR=DOCKER cargo test
-
 dev:
 	make build; RUST_LOG=debug make run
 
@@ -37,3 +25,15 @@ lint:
 	cd frontend; npm run lint; cd ..; \
 	cd backend; cargo fmt; cargo clippy; cd ..; \
 	shellcheck *.sh;
+
+docker_run:
+	cd backend/migrations; \
+	diesel migration run; \
+	cd ../../backend; \
+	RUST_LOG=debug cargo run
+
+docker_test:
+	cd backend/migrations; \
+	diesel migration run; \
+	cd ../../backend; \
+	RUST_LOG=debug cargo test
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,38 +5,35 @@ use crate::database::establish_connection;
 use actix_files::Files;
 use actix_identity::IdentityMiddleware;
 use actix_session::{storage::CookieSessionStore, SessionMiddleware};
-use actix_web::{
-    http::header::LOCATION, middleware, web, App, HttpResponse, HttpServer, Responder,
-};
+use actix_web::{web, App, HttpServer};
 use cookie::Key;
+use tracing_actix_web::TracingLogger;
 
 pub mod database;
 pub mod schema;
 
-// #[catch(404)]
+/*
+#[catch(404)]
 pub async fn error_404() -> impl Responder {
     HttpResponse::PermanentRedirect()
         .insert_header((LOCATION, "/errors/error_404.html"))
         .finish()
 }
+*/
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    let secret_key = Key::generate();
+    init_telemetry();
 
-    let ip_addr = match std::env::var("ADDR").unwrap_or("LOCAL".to_string()).as_str() {
-        "DOCKER" => { "0.0.0.0" },
-        _ => { "127.0.0.1" },
-    };
+    let secret_key = Key::generate();
 
     let pool = establish_connection();
     log::info!("database connection established");
-    log::info!("starting HTTP server at http://{}:8080", ip_addr);
+    log::info!("starting HTTP server at http://0.0.0.0:8080");
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
-            .wrap(middleware::Logger::default())
+            .wrap(TracingLogger::default())
             .wrap(IdentityMiddleware::default())
             .wrap(
                 SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
@@ -51,20 +48,65 @@ async fn main() -> std::io::Result<()> {
                     .index_file("index.html"),
             )
     })
-    .bind((ip_addr, 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
+}
+
+fn init_telemetry() {
+    use std::{fs::File, sync::Arc};
+    use tracing_subscriber::{filter, prelude::*};
+
+    let stdout_log = tracing_subscriber::fmt::layer().pretty();
+
+    // A layer that logs events to a file.
+    let file = File::create("debug.log");
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => panic!("Error: {:?}", error),
+    };
+    let debug_log = tracing_subscriber::fmt::layer().with_writer(Arc::new(file));
+
+    // A layer that collects metrics using specific events.
+    let metrics_layer = /* ... */ filter::LevelFilter::INFO;
+
+    tracing_subscriber::registry()
+        .with(
+            stdout_log
+                // Add an `INFO` filter to the stdout logging layer
+                .with_filter(filter::LevelFilter::INFO)
+                // Combine the filtered `stdout_log` layer with the
+                // `debug_log` layer, producing a new `Layered` layer.
+                .and_then(debug_log)
+                // Add a filter to *both* layers that rejects spans and
+                // events whose targets start with `metrics`.
+                .with_filter(filter::filter_fn(|metadata| {
+                    !metadata.target().starts_with("metrics")
+                })),
+        )
+        .with(
+            // Add a filter to the metrics label that *only* enables
+            // events whose targets start with `metrics`.
+            metrics_layer.with_filter(filter::filter_fn(|metadata| {
+                metadata.target().starts_with("metrics")
+            })),
+        )
+        .init();
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use actix_web::test;
+    use serial_test::serial;
 
     #[actix_web::test]
+    #[serial]
     async fn test_index_page() {
+        init_telemetry();
+
         let app = test::init_service(
-            App::new().wrap(middleware::Logger::default()).service(
+            App::new().wrap(TracingLogger::default()).service(
                 Files::new("/", "../frontend/dist")
                     .prefer_utf8(true)
                     .index_file("index.html"),
